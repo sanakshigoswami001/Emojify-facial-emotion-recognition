dadf26a46f168de5ba7c689a7e80460168f27566
gaurav singh <gaurav176086@gmail.com>
2024-08-28 14:40:32 +0530

    all files are added

---
+++ train.py
@@ -0,0 +1,83 @@
+import numpy as np
+import cv2 as cv
+from tensorflow.keras.models import Sequential, load_model  # type: ignore
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D
+from keras.optimizers import Adam
+from keras.layers import MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+
+train_dir = "D://Project//2//data//train"
+val_dir = "D://Project//2//data//test"
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(48,48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+validation_generator = val_datagen.flow_from_directory(
+        val_dir,
+        target_size=(48,48),
+        batch_size=64,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+emotion_model = Sequential()
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))
+
+cv.ocl.setUseOpenCL(False)
+
+emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+
+emotion_model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
+
+emotion_model_info = emotion_model.fit_generator(
+        train_generator,
+        steps_per_epoch=28709 // 64,
+        epochs=50,
+        validation_data=validation_generator,
+        validation_steps=7178 // 64)
+emotion_model.save_weights('emotion_model.h5')
+
+# start the webcam feed
+cap = cv.VideoCapture(0)
+while True:
+    # Find haar cascade to draw bounding box around face
+    ret, frame = cap.read()
+    if not ret:
+        break
+    bounding_box = cv.CascadeClassifier('C:/Users/hp/AppData/Roaming/Python/Python310/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    num_faces = bounding_box.detectMultiScale(gray_frame,scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in num_faces:
+        cv.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+        roi_gray_frame = gray_frame[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv.resize(roi_gray_frame, (48, 48)), -1), 0)
+        emotion_prediction = emotion_model.predict(cropped_img)
+        maxindex = int(np.argmax(emotion_prediction))
+        cv.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv.LINE_AA)
+
+    cv.imshow('Video', cv.resize(frame,(1200,860),interpolation = cv.INTER_CUBIC))
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroy
